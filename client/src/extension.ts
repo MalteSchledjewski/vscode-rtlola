e09d2613b0430fa2b667a6c0b04edb5700618977
Malte Schledjewski <schledjewski@react.uni-saarland.de>
2023-10-17 09:26:08 +0200

    only load the LS once

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,10 +3,7 @@ import {
   workspace as Workspace,
   window as Window,
   ExtensionContext,
-  TextDocument,
   OutputChannel,
-  WorkspaceFolder,
-  Uri,
 } from "vscode";
 
 import {
@@ -16,56 +13,32 @@ import {
 } from "vscode-languageclient/node";
 
 let defaultClient: LanguageClient;
-const clients: Map<string, LanguageClient> = new Map();
 
 export function activate(context: ExtensionContext) {
   const module = context.asAbsolutePath(path.join("dist", "server.js"));
-  console.log(module);
   const outputChannel: OutputChannel = Window.createOutputChannel("rtlola-lsp");
-
-  function didOpenTextDocument(document: TextDocument): void {
-    // We are only interested in language mode text
-    if (
-      document.languageId !== "rtlola" ||
-      (document.uri.scheme !== "file" && document.uri.scheme !== "untitled")
-    ) {
-      return;
-    }
-
-    const uri = document.uri;
-    const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-    const serverOptions = {
-      run: { module, transport: TransportKind.ipc },
-      debug: { module, transport: TransportKind.ipc, options: debugOptions },
-    };
-    const clientOptions: LanguageClientOptions = {
-      documentSelector: [
-        { scheme: "untitled", language: "rtlola" },
-        { scheme: "file", language: "rtlola" },
-      ],
-      diagnosticCollectionName: "rtlola-lsp",
-      outputChannel: outputChannel,
-    };
-    defaultClient = new LanguageClient(
-      "rtlola-lsp",
-      "RTLola Language Server",
-      serverOptions,
-      clientOptions
-    );
-    defaultClient.start();
-  }
-
-  Workspace.onDidOpenTextDocument(didOpenTextDocument);
-  Workspace.textDocuments.forEach(didOpenTextDocument);
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+  const serverOptions = {
+    run: { module, transport: TransportKind.ipc },
+    debug: { module, transport: TransportKind.ipc, options: debugOptions },
+  };
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { scheme: "untitled", language: "rtlola" },
+      { scheme: "file", language: "rtlola" },
+    ],
+    diagnosticCollectionName: "rtlola-lsp",
+    outputChannel: outputChannel,
+  };
+  defaultClient = new LanguageClient(
+    "rtlola-lsp",
+    "RTLola Language Server",
+    serverOptions,
+    clientOptions
+  );
+  defaultClient.start();
 }
 
 export function deactivate(): Thenable<void> {
-  const promises: Thenable<void>[] = [];
-  if (defaultClient) {
-    promises.push(defaultClient.stop());
-  }
-  for (const client of clients.values()) {
-    promises.push(client.stop());
-  }
-  return Promise.all(promises).then(() => undefined);
+  return defaultClient.stop();
 }
